c439b8b126b8dc4e4010860392984e85489899fa
Mahx2019 <1491657425@qq.com>
2022-01-16 22:39:29 +0800

    清除test

--- test.py
+++ test.py
@@ -1,11 +1,1 @@
 # encoding:utf-8
-from src.parameters_list import *
-from src.toolfunc import resLinerFunc, roundness
-from material_selection import MS
-
-ms1 = MS()
-print(ms1.name)
-print(ms1.sigma_s)
-print(ms1.hardness)
-sigma_Hlim = resLinerFunc(Fzj[0][0],Fzj[0][1],Fzj[0][2],Fzj[0][3],ms1.hardness)
-print(roundness(sigma_Hlim))
