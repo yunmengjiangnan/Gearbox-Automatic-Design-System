f2ce36c7c8d6755b315759eeb900845d7445a8c0
yunmengjiangnan <347036740@qq.com>
2022-02-24 13:33:53 +0800

    update

--- end_cap.py
+++ end_cap.py
@@ -8,4 +8,9 @@ from rich.console import Console
 # 实例化一个输出控制端
 console = Console()
 
-print()
+
+class HighSpeedEndCap:
+    def __init__(self):
+        self.D =
+        console.print('参照参考文献[2]P166表11-10', style='green')
+        console.print('1、高速轴轴承盖设计', style='#FF6100')
